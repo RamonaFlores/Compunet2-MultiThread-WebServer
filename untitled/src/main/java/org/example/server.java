b5a7ce0fb39ea3b9fa317a928f0b6856ee2fba4a
Ramona Flores <ramonaflores@Ramonas-MacBook-Pro.local>
2025-08-05 12:08:14 -0500

    Repo initialization

---
+++ untitled/src/main/java/org/example/server.java
@@ -0,0 +1,106 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+    public void init() throws IOException{
+        ServerSocket serverSocket = new ServerSocket(8050);
+        var isAlive = true;
+        while (isAlive) {
+            System.out.println("Esperando cliente...");
+            var socket = serverSocket.accept();
+            System.out.println("¡Cliente conectado!");
+            dispatchWorker(socket);
+
+        }
+    }
+    public void dispatchWorker(Socket socket) throws IOException {
+        new Thread(
+                ()-> {
+                    try {
+                        handleRequest(socket);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ).start();
+    }
+
+    public void handleRequest(Socket socket) throws IOException {
+        //Lo que recibimos de chrome
+        var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        //Lo que enviamos a chrome
+        var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            System.out.println(line);
+            if (line.startsWith("GET")){
+                //esto se podría utilizar con un string tokenizer
+                //el string tokenizer es una lista enlazada con
+                //referencias next next next
+               var resource =line.split(" ")[1].replace("/", "");
+               System.out.println("Client is asking for:"+ resource);
+
+               //Enviar la response
+                sendResponse(socket,resource);
+            }
+
+        }
+    }
+    public void sendResponse(Socket socket, String resource) throws IOException {
+        var file = new File("");
+        System.out.println(file.getAbsolutePath());
+        var res = new File("resources/" + resource);
+        System.out.println(res.getAbsolutePath());
+
+        if (res.exists()) {
+
+
+            var fis = new FileInputStream(res);
+            var br = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            StringBuilder response = new StringBuilder("<html><body><h1>Hola a todos</h1></body></html>");
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            //Response
+
+            writer.write("HTTP/1.1 200 OK\r\n");
+            writer.write("Content-Type: text/html\r\n");
+            writer.write("Content-Length: " + response.length() + "\r\n");
+            writer.write("Connection: close\r\n");
+            writer.write("\r\n");
+            writer.write(response.toString());
+
+            writer.close();
+            socket.close();
+
+        } else {
+            System.out.println("no se encuentra ningún archivo");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        server s = new server();
+        s.init();
+
+            /*
+            //Response
+            var response = "<html><body><h1>Hola a todos</h1></body></html>";
+            writer.write("HTTP/1.1 200 OK\r\n");
+            writer.write("Content-Type: text/html\r\n");
+            writer.write("Content-Length: " + response.length()+"\r\n");
+            writer.write("Connection: close\r\n");
+            writer.write("\r\n");
+            writer.write(response);
+
+            writer.close();
+            socket.close();
+
+             */
+
+    }
+}
